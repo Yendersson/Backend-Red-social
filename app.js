51f8619294c9d0fb348725160d9ddfff2ade3827
yenderson <yendersoncolmenares67@gmail.com>
2022-09-25 19:43:15 -0300

    nueva prueba

--- app.js
+++ app.js
@@ -17,11 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    res.json('Hello wor')
-})
-
-
+// app.get('/', (req, res) => {
+//     res.send('Hello word');
+// })
 
 DB.connect();
 
