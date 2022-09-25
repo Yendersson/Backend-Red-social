a65aaac883f75cc7b67f9ef5bb0f53b443365a95
yenderson <yendersoncolmenares67@gmail.com>
2022-09-25 17:32:44 -0300

    config DB

--- connectDB.js
+++ connectDB.js
@@ -2,10 +2,14 @@ import 'dotenv/config.js'
 import sql from 'mysql';
 
 const con = sql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '95790078Yc*',
-    database: 'postest'
+    host:'us-cdbr-east-06.cleardb.net',
+    user: 'b45275e215bfec',
+    password: '18f09d94',
+    database: 'heroku_74b6a8f406e6604'
+    // host: 'localhost',
+    // user: 'root',
+    // password: '95790078Yc*',
+    // database: 'postest'
 });
 
 const connect = ()=>{
