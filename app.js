35585a5658fbc0288b5d7c3f64a2fddfe30e9d1a
yenderson <yendersoncolmenares67@gmail.com>
2022-09-25 17:50:16 -0300

    prueba en DB

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    res.json('Hello world')
-})
+// app.get('/', (req, res) => {
+//     res.json('Hello wor')
+// })
 
 
 
-DB.connect();
+// DB.connect();
 
 app.use('/auth', routerAuth);
 app.use('/profile', routerProfile);
