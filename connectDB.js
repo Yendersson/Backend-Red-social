84b2808b8923a0683d7b046bdaa29a42f3f890eb
yenderson <yendersoncolmenares67@gmail.com>
2022-09-25 21:30:06 -0300

    config .js

--- connectDB.js
+++ connectDB.js
@@ -1,6 +1,6 @@
 // import 'dotenv/config.js'
 import sql from 'mysql';
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
 const con = sql.createConnection({
     host:DB_HOST,
