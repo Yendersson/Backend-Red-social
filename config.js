e1680c4639fa72c190dff807f70620591d4a62b6
yenderson <yendersoncolmenares67@gmail.com>
2022-10-05 11:06:24 -0300

    nueva conexion a la DB

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 8080;
-export const DB_HOST = 'sql10.freesqldatabase.com' || 'localhost';
-export const DB_USER = 'sql10522378' || 'root';
-export const DB_PASSWORD = 'HWVgAM1T8J' || '95790078Yc*';
-export const DB_NAME = 'sql10522378' || 'postest';
+export const DB_HOST = 'bjza3jclxjbuu5d4fwqn-mysql.services.clever-cloud.com' || 'localhost';
+export const DB_USER = 'ud1ooqvzwbdswkeh' || 'root';
+export const DB_PASSWORD = 'jL4V8T5bsZy4EMibTL12' || '95790078Yc*';
+export const DB_NAME = 'bjza3jclxjbuu5d4fwqn' || 'postest';
 export const DB_PORT = process.env.DB_PORT || '3306';
